ec13f6a7ea1b9a646a63f64f6decc68717620f4e
aljimsondev <aljimson.megrino@gmail.com>
2026-02-22 08:17:42 +0800

    Chore | Added custom logger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config';
 import app from './app';
+import { Logger } from './utils/logger';
 
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`Shopify API running on PORT ${port}`);
+  Logger.custom(`Shopify API running on PORT ${port}`, {
+    type: 'READY',
+    color: 'CYAN',
+    mode: 'background',
+  });
 });
